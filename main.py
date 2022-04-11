16a9598bfaa3e2babb28b552ceebbafd473c5a97
palmerc120 <palmerc120@gmail.com>
2022-04-10 21:52:47 -0700

    Update description

--- main.py
+++ main.py
@@ -2,12 +2,19 @@
 # Program Filename: ENGR-103-HW1.py
 # Author:           Palmer Campbell-Kaswell
 # Last updated:     4/8/22
-# Description:
+# Description:      This program calculates the power output of a wind
+#                   turbine, given its radius, the average wind speed,
+#                   and the operating efficiency of the turbine. It
+#                   then estimates how many turbines would be required
+#                   to power one person's life in Oregon.
 # Input:            1. Turbine blade radius
 #                   2. Average wind speed
 #                   3. Turbine operating efficiency
+#                   4. Would the user like to run the program again?
 # Output:           1. Maximum available power
 #                   2. Actual power output
+#                   3. The number of turbines required to power one
+#                      person's life for a year in Oregon
 #######################################################################
 
 import math
@@ -16,17 +23,18 @@ JOULES_PER_CAPITA_OR = 2.57e10
 YEARS_PER_SECOND = 1 / (60 * 60 * 24 * 365)
 
 #######################################################################
-# Function: get_numeric_input
-# Description: Retrieves numeric input and checks for errors. Prompts
-#              user to try again if an error occurs.
-# Parameters: message - a string prompting the user to input a value
-#             input_name - a string indicating what variable the user
-#                          entered. Used to make error messages input-
-#                          specific.
-# Return values: The user's input converted to a float
-# Pre-Conditions: The user input a number
-# Post-Conditions: The input has been checked for errors and converted
-#                  to a float
+# Function name:    get_numeric_input
+# Description:      Retrieves numeric input and checks for errors.
+#                   Prompts user to try again if an error occurs.
+# Parameters:       message - a string prompting the user to input a
+#                             value
+#                   input_name - a string indicating what variable the
+#                                user entered. Used to make error
+#                                messages input-specific.
+# Return values:    The user's input converted to a float
+# Pre-Conditions:   The user input a number
+# Post-Conditions:  The input has been checked for errors and converted
+#                   to a float.
 #######################################################################
 
 
@@ -95,9 +103,10 @@ def get_numeric_input(message, input_name):
 
 
 print("\nThis program calculates the power output of a wind turbine, given its radius, the average wind speed, and the"
-      "\noperating efficiency of the turbine. The density of air is assumed to be 1.2 kg/m\u00b2. All values must be"
-      " given \nas either an integer, a decimal, or a fraction. Scientific notation is not supported at this time. "
-      "Units do \nnot need to be included. \n")
+      "\noperating efficiency of the turbine. It then estimates how many turbines would be required to power one\n"
+      "person's life in Oregon. The density of air is assumed to be 1.2 kg/m\u00b2. All values must be given as "
+      "either\nan integer, a decimal, or a fraction. Scientific notation is not supported at this time. "
+      "Units do not need \nto be included. \n")
 
 while True:
     rho = 1.2
@@ -119,11 +128,13 @@ while True:
         else:
             break
 
+    # calculate results
     area = math.pi * radius ** 2
     max_power = 0.5 * rho * area * windspeed ** 3
     actual_power = efficiency * max_power
     num_turbines_per_person_OR = JOULES_PER_CAPITA_OR * (1 / actual_power) * YEARS_PER_SECOND
 
+    # output results to the user
     print("\nTurbine area: ", round(area, 2), "m\u00b2")
     print("Maximum available power:", round(max_power, 2), "W")
     print("Actual power output:", round(actual_power, 2), "W")
